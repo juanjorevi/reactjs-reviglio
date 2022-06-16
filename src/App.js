ed4f6b0b82f2f7e202cac7fa0143735284207870
juanjo <jjreviglio@gmail.com>
2022-06-16 08:20:22 -0300

    proyectoreactjsreviglio

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const App = () =>{
+  return(
+    <>
+    <img src="" alt=""/>
+    <h1>hola guacho</h1>
+    <nav>
+      <a href="">cater 1 </a>
+      <a href="">cater 2 </a>
+      <a href="">cater 3</a>
+    </nav>
+    </>
+  )
+}
+
+export default App;
+//import logo from './logo.svg';
+//import './App.css';
+//
+//function App() {
+//  return (
+//    <header>contenido header</header>
+//  );
+//}
+//
